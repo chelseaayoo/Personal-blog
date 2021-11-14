1b66db3815350228a67128059a31831deea98995
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-14 15:49:07 -0500

    add function that gets response to url request

--- app/request.py
+++ app/request.py
@@ -10,21 +10,20 @@ def configure_request(app):
     global base_url
     base_url = app.config["QUOTES_API_BASE_URL"]
 
-# def get_quotes():
-#     '''
-#     Function that gets the json response to our url request
-#     '''
-#     get_quotes_url = base_url
+def get_quotes():
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_quotes_url = base_url
 
-#     with urllib.request.urlopen(get_quotes_url) as url:
-#         get_quotes_data = url.read()
-#         get_quotes_response = json.loads(get_quotes_data)
+    with urllib.request.urlopen(get_quotes_url) as url:
+        get_quotes_data = url.read()
+        get_quotes_response = json.loads(get_quotes_data)
 
-#         quotes_results = None
+        quotes_results = None
 
-#         if get_quotes_response:
-#             quote_results_list = get_quotes_response
-#             quote_results = quote_results_list            
+        if get_quotes_response:
+            quote_results_list = get_quotes_response
+            quote_results = quote_results_list            
 
-
-#     return quote_results
+    return quote_results
