b8059f618004535543794a7884d4dc20e886e5c7
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-15 06:56:24 -0500

    create class loginform

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -16,7 +16,7 @@ class RegistrationForm(FlaskForm):
 
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('That username is taken')
+            raise ValidationError('That username is already  taken')
 
 class LoginForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
