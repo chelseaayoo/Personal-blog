c8dacd0855c036047f3b8061a138491f72edac1c
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-15 02:41:10 -0500

    create class updateprofileform

---
+++ app/main/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,TextAreaField
+from wtforms.validators import Required,Email,EqualTo
+
+
+class UpdateProfileForm(FlaskForm):
+    bio = TextAreaField('Give more information aboit you.',validators = [Required()])
+    submit = SubmitField('Update bio')
+
+class UploadBlogForm(FlaskForm):
+    title = TextAreaField('Blog Title',validators=[Required()])
+    blog =  TextAreaField('your blog',validators=[Required()])
+    submit = SubmitField('Post')
+
+class CommentsForm(FlaskForm):
+    comment = TextAreaField('Type comment:',validators=[Required()])
+    submit = SubmitField('Comment')
+    
