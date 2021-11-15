b2ed70a8b1ea6dc13cf4711a5c8c0a3f141696da
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-15 06:27:57 -0500

    get mail message

--- app/email.py
+++ app/email.py
@@ -3,6 +3,8 @@ from flask import render_template
 from . import mail
 
 def mail_message(subject,template,to,**kwargs):
+    # """
+    # Get mail message"""
     sender_email = 'chelsea.ayoo@student.moringaschool.com'
 
     email = Message(subject, sender=sender_email, recipients=[to])
